79f2b98ec988934d8efd25d121dda1e6b63e376a
Jonathan Ato Markin <jona10711@gmail.com>
2023-09-04 12:03:13 +0200

    Head added

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+    return (
+        <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href='/'>Back to home</Link>
+            </h2>
+        </>
+    );
 }
